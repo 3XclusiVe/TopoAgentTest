983497ce265e390c79f57d56cb15aa933149504f
Дмитрий <malllkin007@gmail.com>
2014-10-14 23:23:54 +0400

    Test

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+//////
+    /**
+     *
+     * @param val
+     * @return
+     */
+    private static String prse ( int val ) {
+        return "da";
+    }
+
+    private static String prsde ( int val ) {
+        return "da";
+    }
+
+
+
+
+
+}
